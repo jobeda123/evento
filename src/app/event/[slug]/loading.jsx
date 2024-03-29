a1db97fdf4417b40f566905f2d89d752f965872a
Jobeda Nur <jobedanur265@gmail.com>
2024-03-30 00:55:15 +0600

    correctly import skeleton component in loading page

--- src/app/event/[slug]/loading.jsx
+++ src/app/event/[slug]/loading.jsx
@@ -1,9 +1,9 @@
-import Skeleton from "@/components/common/Skeleton"
+import Skeleton from "../../../components/common/skeleton"
 
 export default function loading() {
   return (
     <div className="flex flex-col items-center gap-y-4 pt-28 w-full">
-      <Skeleton className="h-8 w-[300px]"/>
+      <Skeleton className="h-8 w-[300px]" />
       <Skeleton />
       <Skeleton />
     </div>
